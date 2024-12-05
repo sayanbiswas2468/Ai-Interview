3f7d52ca93bb1a26afe104ba883c914311eddfcc
Sayan Biswas <sayanbiswas2468@gmail.com>
2024-12-05 10:01:05 +0530

    v1

--- app/page.tsx
+++ app/page.tsx
@@ -33,10 +33,9 @@ const Home = () => {
         alert("An unexpected error occurred. Please try again.");
       }
     } finally {
-      setLoading(false); // Hide loader
+      setLoading(false); 
     }
   };
-
 
 
   return (
